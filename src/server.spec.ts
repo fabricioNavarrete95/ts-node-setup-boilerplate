ddc62b7cb465f975a91ab5466731244782cb9a8c
Fabricio Navarrete <eansteel1995@gmail.com>
2023-09-25 12:11:04 -0500

    chore: add test:staged script to lint-staged

--- src/server.spec.ts
+++ src/server.spec.ts
@@ -1,3 +1,6 @@
-it("should sum", () => {
-  expect(1 + 1).toBe(2);
+import Logger from "./server";
+
+it("should log and mismatch message", () => {
+  const message = "hello world!";
+  expect(Logger.log(message)).toBe("hello world!");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
-const message = "hello world!";
+class Logger {
+  static log(message: string) {
+    console.log(message);
+    return message;
+  }
+}
 
-console.log(message);
+export default Logger;
